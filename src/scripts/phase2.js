21f08b698a97034228b88ad4a55a2b077766afa8
neelkirit <neelkirit2@gmail.com>
2019-03-16 22:29:44 -0400

    Phase 2 code

---
+++ src/scripts/phase2.js
@@ -0,0 +1,7 @@
+var rellax = new Rellax('.rellax');
+
+var jarallax = new Jarallax();
+jarallax.addAnimation('#medal',
+    [{progress: '0%',  right: '-5%'},
+    {progress: '20%',  right: '10%'}]);
+
